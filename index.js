3fd01b74179f3b02f5b63a518fadf9fd9d0c40c6
Brian McVeigh <bcmcveigh@gmail.com>
2017-12-19 17:01:55 -0500

    Get stock price for current date.

--- index.js
+++ index.js
@@ -8,10 +8,40 @@ googleFinance.companyNews({
  
 googleFinance.historical({
   symbol: 'NASDAQ:AAPL',
-  from: '2014-01-01',
-  to: '2014-12-31'
+  from: getYesterdaysDate(),
+  to: getCurrentDate(),
 }, function (err, quotes) {
   console.log(quotes);
 });
 
+function getCurrentDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
 
+  var yyyy = today.getFullYear();
+  if(dd<10) {
+    dd='0'+dd;
+  } 
+  if(mm<10){
+    mm='0'+mm;
+  } 
+  return yyyy + '-' + mm + '-' + dd;
+}
+
+function getYesterdaysDate() {
+  var today = new Date();
+  var dd = today.getDate() - 1;
+
+  // todo: polish this logic. Some months may have less than 31 days.
+  var mm = today.getMonth()+1; //January is 0!
+
+  var yyyy = today.getFullYear();
+  if(dd<10) {
+    dd='0'+dd;
+  } 
+  if(mm<10) {
+    mm='0'+mm;
+  } 
+  return yyyy + '-' + mm + '-' + dd;
+}
